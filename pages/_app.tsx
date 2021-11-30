4be0b64ba9d28d4e5d928bc60eb3c74462b69db3
Hasnath abdullah aknd <abdullahaknd297@gmail.com>
2021-11-30 16:42:57 +0600

    initialize

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from "next/app";
+import type { FunctionComponent } from "react";
+
+const App: FunctionComponent<AppProps> = ({
+  Component,
+  pageProps,
+  children,
+}) => {
+  return <Component {...pageProps}>{children}</Component>;
+};
+
+export default App;
