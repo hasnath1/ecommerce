fbf4bae5fd059a726f436d0bfc3bb84ee6684d24
Hasnath abdullah aknd <abdullahaknd297@gmail.com>
2021-12-07 00:33:28 +0600

    settting up custom nextjs server

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,18 @@
+import express from "express";
 import next from "next";
+
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
+const app = express();
+const nextApp = next({ dev: true });
+
+(async () => {
+  await nextApp.prepare();
+  const handle = nextApp.getRequestHandler();
+
+  app.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  app.listen(3000, () => {});
+})();
