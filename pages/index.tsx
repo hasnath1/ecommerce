96beea97aabbb86939880cdf6e2186b2951bfa1d
Hasnath abdullah aknd <abdullahaknd297@gmail.com>
2021-12-07 00:22:38 +0600

    fixed the problem of nav icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,7 @@ import Footer from "../components/Footer/Footer";
 const Index = () => {
   return (
     <div>
-      <Navbar></Navbar>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-      laboriosam iure porro corporis dolore assumenda hic odit soluta. Ipsa
-      doloremque dolorem unde maxime quae temporibus aut sunt harum perferendis
-      ratione. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-      architecto veritatis dolor laborum iure, maiores commodi cumque sapiente
-      unde nam dicta accusantium, autem eligendi, quibusdam incidunt amet fuga
-      perferendis voluptate?
+      <Navbar />
       <Footer />
     </div>
   );
